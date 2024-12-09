3fb9cebe244d16de0e9552aaf9ece5622c661220
José Luiz <jlnalbertii@gmail.com>
2024-12-09 17:41:24 -0300

    Order

---
+++ src/test/java/com/projeto/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
