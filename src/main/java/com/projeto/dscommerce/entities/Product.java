2e2c02746ff3e259a45772f0c3a932b29f987278
José Luiz <jlnalbertii@gmail.com>
2024-12-10 16:22:42 -0300

    OrderItem and OrderItemPK

--- src/main/java/com/projeto/dscommerce/entities/Product.java
+++ src/main/java/com/projeto/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.projeto.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -23,7 +24,10 @@ public class Product {
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories = new HashSet<Category>();
+    private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -78,5 +82,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
